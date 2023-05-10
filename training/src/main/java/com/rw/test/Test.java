5beda18dbb4539f80d633eae22f20d272d404c2a
hyq <356496672@qq.com>
2023-05-10 18:13:52 +0800

    HiCloudNoteServerTraining 项目代码引入

---
+++ training/src/main/java/com/rw/test/Test.java
@@ -0,0 +1,15 @@
+package com.rw.test;
+
+import org.json.JSONArray;
+import org.json.JSONString;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("123456");
+
+    }
+}
